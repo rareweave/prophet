25262413801515678cf2f84b4d265ff8abc6f01d
Nik Rykov <rykov.nikas@gmail.com>
2023-03-17 17:02:26 +0100

    cors + new contract

--- app.js
+++ app.js
@@ -11,6 +11,16 @@ module.exports = async function (fastify, opts) {
   // This loads all plugins defined in plugins
   // those should be support plugins that are reused
   // through your application
+  fastify.addHook("preHandler", (req, res, done) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    const isPreflight = /options/i.test(req.method);
+    if (isPreflight) {
+      return res.send();
+    }
+    done()
+  })
   fastify.register(AutoLoad, {
     dir: path.join(__dirname, 'plugins'),
     options: Object.assign({}, opts)
