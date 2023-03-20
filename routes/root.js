b466a95d0d02237aa4d2ec23ad565c6e2f113409
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-03-20 18:11:56 +0300

    Changes in root.js
    Added try/catch to handle any errors that may occur during the request/response cycle. 
    
    Added error handling to ensure that if the gateway returns a status other than 200 or 202, we throw an error and return a response with status code 500 and an error message. 
    
    Optimised the if statement condition by using the includes method to check that the response status is included in the array of valid statuses.

--- routes/root.js
+++ routes/root.js
@@ -1,14 +1,18 @@
-const { fetch } = require("undici")
-const fs = require("fs")
-const { Readable } = require("stream")
-const config = require("json5").parse(fs.readFileSync("./config.json5"))
+const { fetch } = require("undici");
+const fs = require("fs");
+const { Readable } = require("stream");
+const config = require("json5").parse(fs.readFileSync("./config.json5"));
 module.exports = async function (fastify, opts) {
   fastify.get('/:id', async function (request, reply) {
-    let tx = await fetch(config.arweaveGateway + "/" + request.params.id)
-    if (tx.status != 200 && tx.status != 202) {
-      return { error: "Invalid status from gateway" }
+    try {
+      const tx = await fetch(config.arweaveGateway + "/" + request.params.id);
+      if (![200, 202].includes(tx.status)) {
+        throw new Error("Invalid status from gateway");
+      }
+      reply.raw.writeHead(tx.status, tx.headers);
+      Readable.from(tx.body).pipe(reply.raw);
+    } catch (error) {
+      reply.code(500).send({ error: error.message });
     }
-    reply.raw.writeHead(tx.status, tx.headers)
-    Readable.from(tx.body).pipe(reply.raw)
-  })
-}
+  });
+};
