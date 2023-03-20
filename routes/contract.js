95499df2b8f2cf5656294291cf9fa7c977990640
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-03-20 19:24:37 +0300

    Changes in contract.js
    Added try/catch to catch errors that may occur during endpoint execution, and added console.error to log any errors that may occur during endpoint execution. 
    
    Improved error handling: errors are thrown instead of returning error objects, and error messages have been added for some errors that occur to make debugging easier. 
    
    Added a check to ensure that the HTTP response from the contract initialisation transaction endpoint is OK before proceeding. 
    
    Redesigned code to retrieve the contract source code from the contract initialisation transaction object only once. 
    
    Removed unnecessary encoding and decoding of the contract source code. 
    
    Changed the timestamp property to use toISOString instead of manually constructing the string for better readability and consistency. 
    
    Added a call to contract.getStateHash to retrieve the contract state hash after evaluation.

--- routes/contract.js
+++ routes/contract.js
@@ -1,5 +1,3 @@
-
-
 const fs = require("fs")
 const { fetch } = require("undici")
 const Arweave = require("arweave")
@@ -14,31 +12,42 @@ const warp = WarpFactory.forMainnet({
     inMemory: true,
 }, true, arweave)
 module.exports = async function (fastify, opts) {
-
     fastify.get('/contract', async function (request, reply) {
-        let contractInitTx = await fetch(`http://127.0.0.1:${config.port}/tx/${request.query.id}`).then(res => res.json()).catch(e => null)
-        if (!contractInitTx || contractInitTx.error || !contractInitTx.tags.find(tag => tag.name == Buffer.from("Contract-Src").toString("base64url"))) {
-            reply.status(404)
-            return { error: "No contract found" }
+        try {
+            const contractInitTxRes = await fetch(`http://127.0.0.1:${config.port}/tx/${request.query.id}`);
+            if (!contractInitTxRes.ok) {
+                throw new Error(`Failed to fetch contract initialization transaction: ${contractInitTxRes.statusText}`);
+            }
+            const contractInitTx = await contractInitTxRes.json();
+            const contractSrcTag = contractInitTx.tags.find(tag => tag.name == Buffer.from("Contract-Src").toString("base64url"));
+            if (!contractSrcTag) {
+                reply.status(404);
+                return { error: "No contract found" };
+            }
+            const contractSrc = Buffer.from(contractSrcTag.value, "base64url").toString("utf8");
+            if (!config.whitelistedCodes.includes(contractSrc)) {
+                reply.status(403);
+                return { error: "Code of this contract is not whitelisted for execution by this node" };
+            }
+            const contract = warp.contract(request.query.id);
+            const evaluationResult = await contract.setEvaluationOptions({
+                unsafeClient: "allow",
+                waitForConfirmation: false,
+                remoteStateSyncEnabled: false
+            }).readState();
+            const stateHash = await contract.getStateHash();
+            return {
+                status: "evaluated",
+                contractTxId: request.query.id,
+                state: evaluationResult.cachedValue.state,
+                sortKey: evaluationResult.sortKey,
+                timestamp: new Date(Date.now()).toISOString(),
+                stateHash: stateHash
+            };
+        } catch (e) {
+            console.error(`Error processing contract evaluation request: ${e.message}`);
+            reply.status(500);
+            return { error: "Internal server error" };
         }
-
-        if (!config.whitelistedCodes.includes(Buffer.from(contractInitTx.tags.find(c => c.name == Buffer.from("Contract-Src").toString("base64url")).value, "base64url").toString("utf8"))) {
-            reply.status(403)
-            return { error: "Code of this contract is not whitelisted for execution by this node" }
-        }
-        let contract = warp.contract(request.query.id)
-        let evalutionResult = await (contract.setEvaluationOptions({
-            unsafeClient: "allow", waitForConfirmation: false, //we are using anchoring
-            remoteStateSyncEnabled: false
-        })).readState()
-        let time = Date.now()
-        return {
-            "status": "evaluated",
-            contractTxId: request.query.id,
-            state: evalutionResult.cachedValue.state,
-            sortKey: evalutionResult.sortKey,
-            timestamp: new Date(time).getFullYear() + '-' + new Date(time).getMonth() + '-' + new Date(time).getDay() + ' ' + new Date(time).getHours() + ':' + new Date(time).getMinutes() + ':' + new Date(time).getSeconds(),
-            stateHash: contract.stateHash
-        }
-    })
-}
+    });
+};
