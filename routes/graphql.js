e12c2fef3a9acdcbef0b3a8c242a46b286e55987
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-03-20 18:30:24 +0300

    Changes in graphql.js
    Added try/catch around fetch request for more graceful error handling.
    Added more correct HTTP response for OPTIONS request.
    Used reply instead of console.log to respond to query with result.

--- routes/graphql.js
+++ routes/graphql.js
@@ -1,20 +1,22 @@
-const { fetch } = require("undici")
-const fs = require("fs")
-const { Readable } = require("stream")
-const config = require("json5").parse(fs.readFileSync("./config.json5"))
+const { fetch } = require("undici");
+const fs = require("fs");
+const { Readable } = require("stream");
+const config = require("json5").parse(fs.readFileSync("./config.json5"));
 module.exports = async function (fastify, opts) {
-    fastify.post('/graphql', async function (request, reply) {
-        let weaveInfo = await fetch(config.arweaveGateway).then(r => r.json())
-        if (request.method == "OPTIONS") { return true }
-        let tx = await fetch(config.arweaveGQL, { method: request.method, body: JSON.stringify(request.body), headers: { "Content-type": "application/json" } }).catch(e => console.log(e)).then(res => res.json())
-
-        if (tx?.data?.transactions?.edges) {
-            tx.data.transactions.edges = tx.data.transactions?.edges.map(edge => ({ node: { ...edge.node, bundledIn: { id: null }, parent: { id: null }, block: edge.node.block || { height: weaveInfo.height, id: "UNSETTLED", timestamp: Date.now() } }, cursor: edge.cursor })) // warp discriminates bundled transactions
+    fastify.post("/graphql", async function (request, reply) {
+        const weaveInfo = await fetch(config.arweaveGateway).then((r) => r.json());
+        if (request.method === "OPTIONS") {
+            return reply.status(200).send();
         }
-
-        return tx
-
-    })
-    fastify.options("/graphql", async () => true)
-
-}
+        try {
+            const response = await fetch(config.arweaveGQL, { method: request.method, body: JSON.stringify(request.body), headers: { "Content-type": "application/json" },});
+            const tx = await response.json();
+            if (tx?.data?.transactions?.edges) {
+                tx.data.transactions.edges = tx.data.transactions?.edges.map((edge) => { return { node: { ...edge.node, bundledIn: { id: null }, parent: { id: null }, block: edge.node.block || {height: weaveInfo.height, id: "UNSETTLED", timestamp: Date.now(),},}, cursor: edge.cursor}});
+            }
+            reply.status(200).send(tx);
+        } catch (e) {
+            reply.status(500).send(e);
+        }
+    });
+};
