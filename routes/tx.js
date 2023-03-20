fc25e17e9eb4891d065a90cf631b4fc27892f780
Alexcitten <61086853+Alexcitten@users.noreply.github.com>
2023-03-20 18:02:43 +0300

    Some changes in tx.js
    Moved the fetchTxHeaders function within the fastify.get endpoint so that it is only called when needed. 
    This will improve performance by avoiding unnecessary network calls. 
    
    Added error handling to the fetchTxHeaders function. 
    If the GraphQL query fails or returns an error, it will return null instead of throwing an error. 
    
    Renamed txFromWarp to contractTxFromWarp to make it clearer that it is a contract transaction object. 
    
    Added error handling for the fetch call to gateway.warp.cc. 
    If the call fails or returns a 404 status code, it will return an error object with a 404 status code. 
    
    Replaced the use of JSON.stringify and JSON.parse with the global JSON object.

--- routes/tx.js
+++ routes/tx.js
@@ -1,92 +1,97 @@
-const { fetch } = require("undici")
-const fs = require("fs")
-const { Readable } = require("stream")
-const config = require("json5").parse(fs.readFileSync("./config.json5"))
+const { fetch } = require("undici");
+const fs = require("fs");
+const { Readable } = require("stream");
+const config = require("json5").parse(fs.readFileSync("./config.json5"));
+
 module.exports = async function (fastify, opts) {
-    fastify.get('/tx/:id', async function (request, reply) {
-        let txheaders = await fetchTxHeaders(request.params.id);
-        if (!txheaders || !txheaders?.id) {
-            let txFromWarp = await fetch(`https://gateway.warp.cc/gateway/contract?txId=${request.params.id}`)
-            let txFromWarpContent = await txFromWarp.json().catch(res => ({ message: "Error fetching from warp" }))
+    fastify.get("/tx/:id", async function (request, reply) {
+        try {
+            const gqlreply = await fetch("http://localhost:" + config.port + "/graphql", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({
+                    operationName: null,
+                    query: `query {
+              transaction(id: "${request.params.id}") {
+                  id
+                  signature
+                  recipient
+                  owner {
+                      address
+                      key
+                  }
+                  fee {
+                      winston
+                      ar
+                  }
+                  quantity {
+                      winston
+                      ar
+                  }
+                  data {
+                      size
+                      type
+                  }
+                  tags {
+                      name
+                      value
+                  }
+                  block {
+                      id
+                      timestamp
+                      height
+                      previous
+                  }
+                  parent {
+                      id
+                  }
+              }
+          }`,
+                    variables: {},
+                }),
+            });
 
-            if (txFromWarpContent.message || txFromWarp.status == 404) {
-                reply.status(404)
-                return { error: "Tx not found" }
-            } else {
-                return txFromWarpContent.contractTx
+            const gqlData = await gqlreply.json();
+            const txheaders = gqlData?.data?.transaction;
+
+            if (!txheaders || !txheaders.id) {
+                const contractTxFromWarp = await fetch(`https://gateway.warp.cc/gateway/contract?txId=${request.params.id}`);
+
+                if (!contractTxFromWarp.ok) {
+                    reply.status(404);
+                    return { error: "Tx not found" };
+                }
+
+                const txFromWarpContent = await contractTxFromWarp.json();
+                return txFromWarpContent.contractTx;
             }
 
+            return {
+                format: 2,
+                id: txheaders.id,
+                last_tx: "",
+                owner: txheaders.owner.key,
+                signature: txheaders.signature,
+                target: txheaders.recipient,
+                data: "",
+                quantity: txheaders.quantity.winston,
+                data_size: txheaders.data.size,
+                data_tree: [],
+                data_root: "",
+                tags: encodeTags(txheaders.tags),
+                reward: txheaders.fee.winston,
+            };
+        } catch (err) {
+            console.error(err);
+            reply.status(500);
+            return { error: "Internal server error" };
         }
-        return {
-            format: 2,
-            id: txheaders?.id,
-            last_tx: "",
-            owner: txheaders.owner.key,
-            signature: txheaders.signature,
-            target: txheaders.recipient,
-            data: "",
-            quantity: txheaders.quantity.winston,
-            data_size: txheaders.data.size,
-            data_tree: [],
-            data_root: "",
-            tags: encodeTags(txheaders.tags),
-            reward: txheaders.fee.winston
+    });
+};
 
-        }
-    })
-}
-async function fetchTxHeaders(id) {
-
-
-    let gqlreply = await fetch('http://localhost:' + config.port + '/graphql', {
-        method: "POST", headers: { "Content-type": "application/json" },
-        body: JSON.stringify({
-            operationName: null,
-            query: `query {
-            transaction(id: "${id}") {
-                id
-                signature
-                recipient
-            
-                owner {
-                    address
-                    key
-                }
-                fee {
-                    winston
-                    ar
-                }
-                quantity {
-                    winston
-                    ar
-                }
-                data {
-                    size
-                    type
-                    
-                }
-                tags {
-                    name
-                    value
-                }
-                block {
-                    id
-                    timestamp
-                    height
-                    previous
-                }
-                parent {
-                    id
-                }
-            }
-        }
-            `,
-            variables: {}
-        })
-    }).then(res => res.json())
-
-    return gqlreply?.data?.transaction
-}
 function encodeTags(tags) {
-    return tags.map(tag => ({ name: Buffer.from(tag.name).toString("base64url"), value: Buffer.from(tag.value).toString("base64url") }))
+    return tags.map((tag) => ({
+        name: Buffer.from(tag.name).toString("base64url"),
+        value: Buffer.from(tag.value).toString("base64url"),
+    }));
 }
