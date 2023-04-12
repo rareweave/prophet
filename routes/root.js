f154aabe6c401cb194a22ab5e314e86236e5ca99
Nik Rykov <rykov.nikas@gmail.com>
2023-04-12 08:50:12 +0200

    options, cors

--- routes/root.js
+++ routes/root.js
@@ -15,21 +15,29 @@ module.exports = async function (fastify, opts) {
     })
     return Buffer.from(await tx.arrayBuffer())
   })
+  fastify.options("/tx", async (req, resp) => {
+    resp.status(200)
+    return
+  })
   fastify.post("/tx", async (request, reply) => {
     let peers = [...(await fastify.kv.get("peers") || []), config.arweaveGateway].map(p => (p.startsWith("http://") || p.startsWith("https://")) ? p : `http://${p}`)
     peers.map(async peer => {
       fetch(`${peer}/tx`, { method: 'POST', headers: { 'Content-Type': "application/json" }, body: JSON.stringify(request.body) }).catch(e => null)
     })
+    reply.header("Acess-Control-Allow-Origin", "*")
     reply.status(200)
     return "OK"
   })
   fastify.get(`/wallet/:wallet/balance`, async (request, reply) => {
+    reply.header("Acess-Control-Allow-Origin", "*")
     return fetch(`${config.arweaveGateway}/wallet/${request.params.wallet}/balance`).then(res => res.text()).catch(e => 0)
   })
   fastify.get(`/price/:dataSize/:address`, async (request, reply) => {
+    reply.header("Acess-Control-Allow-Origin", "*")
     return fetch(`${config.arweaveGateway}/price/${request.params.dataSize}/${request.params.address}`).then(res => res.text()).catch(e => 0)
   })
   fastify.get(`/price/:dataSize`, async (request, reply) => {
+    reply.header("Acess-Control-Allow-Origin", "*")
     return fetch(`${config.arweaveGateway}/price/${request.params.dataSize}`).then(res => res.text()).catch(e => 0)
   })
 }
