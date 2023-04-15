bcbccf0a95ff78b01bf84d1776b4adf7fa7a32e1
Nik Rykov <rykov.nikas@gmail.com>
2023-04-15 03:22:51 +0200

    add chunk uploads

--- routes/root.js
+++ routes/root.js
@@ -19,7 +19,20 @@ module.exports = async function (fastify, opts) {
     resp.status(200)
     return
   })
+  fastify.options("/chunk", async (req, resp) => {
+    resp.status(200)
+    return
+  })
   fastify.post("/tx", async (request, reply) => {
+    let peers = [...(await fastify.kv.get("peers") || []), config.arweaveGateway].map(p => (p.startsWith("http://") || p.startsWith("https://")) ? p : `http://${p}`)
+    peers.map(async peer => {
+      fetch(`${peer}/tx`, { method: 'POST', headers: { 'Content-Type': "application/json" }, body: JSON.stringify(request.body) }).catch(e => null)
+    })
+    reply.header("Acess-Control-Allow-Origin", "*")
+    reply.status(200)
+    return "OK"
+  })
+  fastify.post("/chunk", async (request, reply) => {
     let peers = [...(await fastify.kv.get("peers") || []), config.arweaveGateway].map(p => (p.startsWith("http://") || p.startsWith("https://")) ? p : `http://${p}`)
     peers.map(async peer => {
       fetch(`${peer}/tx`, { method: 'POST', headers: { 'Content-Type': "application/json" }, body: JSON.stringify(request.body) }).catch(e => null)
