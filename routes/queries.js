4ecf490ba18f5a8ec2207a2d395a693b4e872aee
Nik Rykov <rykov.nikas@gmail.com>
2023-06-09 22:33:58 +0200

    improvement in query speed

--- routes/queries.js
+++ routes/queries.js
@@ -26,19 +26,12 @@ module.exports = async function (fastify, opts) {
         let collectionId = request.query.collection
         let startFrom = parseInt(request.query.startFrom) || 0
         let collection;
-        // if (collectionId) {
-        //     collection = (await fastify.db.query(`SELECT contractTxId, timestamp, state FROM contract:\`${collectionId}\`;`, { collectionSrcIds: config.collectionSrcIds, collectionId: collectionId }).catch(e => []))[0]?.result[0] || {}
-        // }
 
-        let conditions = [ownedBy ? 'state.owner = $ownedBy' : '',
-        search ? '(state.description ~ $search OR state.name ~ $search)' : '',
-        request.query.forSaleOnly ? 'state.forSale = true' : '',
-        collectionId ? `(contract:\`${collectionId}\`.state.items) CONTAINS contractTxId` : '']
-        conditions = conditions.filter(c => c).map((c, ci) => `${ci ? 'AND' : 'WHERE'} ${c}`).join(" ")
-        return (await fastify.db.query(`SELECT contractTxId, timestamp, state, owner FROM nfts ${conditions} LIMIT 100 START ${startFrom};`, { ownedBy: ownedBy, search: search, forSaleOnly: request.query.forSaleOnly, collectionSrcIds: config.collectionSrcIds, collectionId: collectionId, collectionData: collection || [] }))[0]
+        return (await fastify.db.query(`SELECT contractTxId, timestamp, state, owner FROM nfts WHERE ($forSaleOnly IS NULL || state.forSale = true) AND ($ownedBy IS NULL || state.owner = $ownedBy) AND ($search IS NULL || (state.description ~ $search OR state.name ~ $search)) AND ($collectionId is NULL || ((contract:\`${collectionId}\`.state.items) CONTAINS contractTxId)) LIMIT 100 START ${startFrom};`,
+            { ownedBy: ownedBy || null, search: search || null, forSaleOnly: request.query.forSaleOnly || null, collectionSrcIds: config.collectionSrcIds, collectionId: collectionId || null, collectionData: collection || [] }))[0]
     })
     fastify.get("/contract-interactions/:id", async (request, reply) => {
-        let resp = (await fastify.db.query(`SELECT * from contractInteractions WHERE contractId = $contractId`, { contractId: request.params.id }).catch(e => []))[0]?.result || []
+        let resp = (await fastify.db.query(`SELECT * from interactions WHERE contractId = $contractId`, { contractId: request.params.id }).catch(e => []))[0]?.result || []
         return resp;
 
     })
@@ -47,9 +40,8 @@ module.exports = async function (fastify, opts) {
         let search = request.query.search
         let startFrom = parseInt(request.query.startFrom) || 0
 
-        return (await fastify.db.query(`SELECT contractTxId, timestamp, state FROM contract WHERE $collectionSrcIds CONTAINS sourceId ${request.query.ownedBy ? 'AND state.admins CONTAINS $ownedBy' : ''} 
-            ${request.query.search ? 'AND (state.description ~ $search OR state.name ~ $search)' : ''}
-            ORDER BY timestamp DESC LIMIT 100 START ${startFrom};`, { ownedBy, search, collectionSrcIds: config.collectionSrcIds }))[0]
+        return (await fastify.db.query(`SELECT contractTxId, timestamp, state FROM contract WHERE $collectionSrcIds CONTAINS sourceId AND ($ownedBy IS NULL || state.admins CONTAINS $ownedBy) AND ($search IS NULL || (state.description ~ $search OR state.name ~ $search)) ORDER BY timestamp DESC LIMIT 100 START ${startFrom};`,
+            { ownedBy: ownedBy || null, search: search || null, collectionSrcIds: config.collectionSrcIds || null }))[0]
     })
     fastify.get('/collection/:id', async function (request, reply) {
         let collectionId = JSON.parse(JSON.stringify(`"${request.params.id}"`))
